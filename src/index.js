21074f64a11814dc848813de97ddb92362ced8f6
Gabriel Crespo <gabriel.crespo1942@gmail.com>
2021-08-16 21:52:23 -0300

    http verbs

--- src/index.js
+++ src/index.js
@@ -1,15 +1,30 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-app.get('/projects', (request, response) => {
-    return response.json({
-       message: "Hello World",
-    });
+const projects = ["Projeto 1", "Projeto 2", "Projeto 3"];
+
+app.get("/projects", (request, response) => {
+  return response.json(projects);
 });
+
+app.post("/projects", (request, response) => {
+    projects.push('Projeto 4');
+    return response.json(projects);
+});
+
+app.put('/projects', (request, response) => {
+    projects[0] = 'Projects 5';
+    return response.json(projects);
+})
+
+app.delete('/projects', (request, response) => {
+    projects.shift();
+    return response.json(projects);
+})
 
 const port = 3333;
 
 app.listen(port, () => {
-    console.log(`server is running on port ${port}`);
+  console.log(`server is running on port ${port}`);
 });
