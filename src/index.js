b9c5862349e3f09b3aef086c802e0b12709dbf42
Gabriel Crespo <gabriel.crespo1942@gmail.com>
2021-08-16 22:14:32 -0300

    query, route and body params

--- src/index.js
+++ src/index.js
@@ -1,24 +1,43 @@
+const { json } = require("express");
 const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
 const projects = ["Projeto 1", "Projeto 2", "Projeto 3"];
 
 app.get("/projects", (request, response) => {
+  const { title, owner } = request.query;
+
+  console.log(title, owner);
+
   return response.json(projects);
 });
 
 app.post("/projects", (request, response) => {
+    const { owner, title, year } = request.body;
+
+    console.log(owner, title, year);
+
     projects.push('Projeto 4');
     return response.json(projects);
 });
 
-app.put('/projects', (request, response) => {
+app.put('/projects/:id', (request, response) => {
+    const { id } = request.params;
+
+    console.log(id);
+
     projects[0] = 'Projects 5';
     return response.json(projects);
 })
 
-app.delete('/projects', (request, response) => {
+app.delete('/projects/:id', (request, response) => {
+    const { id } = request.params;
+
+    console.log(id);
+
     projects.shift();
     return response.json(projects);
 })
