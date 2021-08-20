38ada6a5cf410fb05b91da9f48cf75f4aa859e67
Gabriel Crespo <gabriel.crespo1942@gmail.com>
2021-08-20 00:06:55 -0300

    migrations, seed and images

---
+++ src/database/seeds/create_items.ts
@@ -0,0 +1,34 @@
+import Knex from "knex";
+
+export async function seed(knex: Knex) {
+     await knex('items').insert([
+      {
+       title: "Papéis e Papelão",
+       image: "papel.png"
+      },
+      {
+       title: "Vidros e Lâmpadas",
+       image: "vidro.png"
+      },
+      {
+       title: "Óleo de Cozinha",
+       image: "oleo.png"
+      },
+      {
+       title: "Resíduos Orgânicos",
+       image: "organico.png"
+      },
+      {
+       title: "Baterias e Pilhas",
+       image: "papel.png"
+      },
+      {
+       title: "Papéis e Papelão",
+       image: "bateria.png"
+      },
+      {
+       title: "Eletrônicos",
+       image: "eletronico.png"
+      },
+     ])
+}
